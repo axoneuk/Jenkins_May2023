141060c32929b514bd4e510dc823a4d6c68852f7
axoneuk <info@axone-tech.uk>
2023-05-27 08:11:51 +0100

    Jira 1234 - First time commit of the project

---
+++ src/test/java/uk/axone/devintest/maven/tests/TestPack1.java
@@ -0,0 +1,28 @@
+package uk.axone.devintest.maven.tests;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestPack1 {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Running test 4");
+    }
+
+}
